c1f59b134fb66c98656488c149a0eb71d1e30240
Teemu Suo-Anttila <teemusa@vaadin.com>
2016-07-01 11:04:36 +0300

    Use Vaadin 7.7 and add ContextMenu add-on to project

--- src/main/java/org/vaadin/teemusa/VaadinSpringBootAddonExampleApplication.java
+++ src/main/java/org/vaadin/teemusa/VaadinSpringBootAddonExampleApplication.java
@@ -3,10 +3,40 @@ package org.vaadin.teemusa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vaadin.addon.contextmenu.ContextMenu;
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
 @SpringBootApplication
 public class VaadinSpringBootAddonExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaadinSpringBootAddonExampleApplication.class, args);
+	}
+}
+
+@SpringUI(path = "")
+@Theme("valo")
+class MyUI extends UI {
+
+	@Override
+	protected void init(VaadinRequest request) {
+		VerticalLayout content = new VerticalLayout();
+		content.setSizeFull();
+
+		ContextMenu menu = new ContextMenu(content, false);
+		menu.addItem("Menu entry", menuItem -> {
+			Notification.show("Menu entry selected!", Type.TRAY_NOTIFICATION);
+		});
+		content.addContextClickListener(event -> {
+			menu.open(event.getClientX(), event.getClientY());
+		});
+
+		setContent(content);
 	}
 }
